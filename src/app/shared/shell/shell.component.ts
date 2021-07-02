0accd7bf3a0df5cb605eb0047f016253159f6b8f
Aníbal <anibal.alvarezg@gmail.com>
2021-07-01 23:31:40 -0400

    Material - SharedModule - ShellComponent

---
+++ src/app/shared/shell/shell.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay, tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-shell',
+  templateUrl: './shell.component.html',
+  styleUrls: ['./shell.component.scss']
+})
+export class ShellComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
+    .pipe(
+      tap(console.log),
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
