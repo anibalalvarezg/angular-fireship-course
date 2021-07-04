0a4b495da2ca49ec5556fca52b5e3745a375b3ed
Aníbal <anibal.alvarezgg@gmail.com>
2021-07-03 23:29:21 -0400

    Firebase config and user module

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true, 
+  firebaseConfig: {
+    apiKey: "AIzaSyAPGBJENzZ0Si6ebLIGHmonrxtAmyYxBdI",
+    authDomain: "app-dev-62cb0.firebaseapp.com",
+    projectId: "app-dev-62cb0",
+    storageBucket: "app-dev-62cb0.appspot.com",
+    messagingSenderId: "858499644099",
+    appId: "1:858499644099:web:f953c34cac98acc8170076",
+    measurementId: "G-91JH9008N0"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAPGBJENzZ0Si6ebLIGHmonrxtAmyYxBdI",
+    authDomain: "app-dev-62cb0.firebaseapp.com",
+    projectId: "app-dev-62cb0",
+    storageBucket: "app-dev-62cb0.appspot.com",
+    messagingSenderId: "858499644099",
+    appId: "1:858499644099:web:f953c34cac98acc8170076",
+    measurementId: "G-91JH9008N0"
+  }
 };
 
 /*
